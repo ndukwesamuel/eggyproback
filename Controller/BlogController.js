0e416593fd6e28bfd81dfe6a6838896769123f0d
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-09-24 15:35:56 +0100

    final fix

--- Controller/BlogController.js
+++ Controller/BlogController.js
@@ -9,6 +9,56 @@ const getallBlog = async (req, res) => {
   }
 };
 
+const getSingleData = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const Blogdata = await BlogData.findById(id);
+    res.status(201).json(Blogdata);
+  } catch (error) {
+    res.status(404).json({ msg: "Data not found" });
+  }
+};
+
+const CreateData = async (req, res) => {
+  const { title, body } = req.body;
+
+  try {
+    const data = await BlogData.create({
+      title,
+      body,
+    });
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(400).json({ msg: "Data not created" });
+  }
+};
+
+const UpdateData = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const oldData = await BlogData.findById(id);
+
+    const Updateblog = await BlogData.findByIdAndUpdate(oldData, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(201).json(Updateblog);
+  } catch (error) {
+    res.status(500).json({ msg: "Blog not found" });
+  }
+};
+
+const Deletedata = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const Deletblog = await BlogData.findByIdAndDelete(id);
+    res.status(201).json({ msg: "Deleted Successfully", data: Deletblog });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
 const Pupulate = (req, res) => {
   const data = new BlogData({
     title: "samheart",
@@ -23,4 +73,8 @@ const Pupulate = (req, res) => {
 module.exports = {
   getallBlog,
   Pupulate,
+  getSingleData,
+  CreateData,
+  UpdateData,
+  Deletedata,
 };
--- routes/BlogrRoutes.js
+++ routes/BlogrRoutes.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getallBlog, Pupulate } = require("../Controller/BlogController");
+const {
+  getallBlog,
+  Pupulate,
+  getSingleData,
+  CreateData,
+  UpdateData,
+  Deletedata,
+} = require("../Controller/BlogController");
 
 router.get("/", getallBlog);
+router.post("/", CreateData);
+
+router.get("/:id", getSingleData);
+router.patch("/:id", UpdateData);
+router.delete("/:id", Deletedata);
+
 // router.get("/", Pupulate);
 
 module.exports = router;
